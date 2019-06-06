6a82f9af4efff7e6811d5601a96513a995855bc0
sqzhang <3257179914@qq.com>
2019-06-07 05:07:15 +0800

    finish the wechat ebooks functions

--- 10.一键生成微信朋友圈电子书/main.py
+++ 10.一键生成微信朋友圈电子书/main.py
@@ -3,169 +3,178 @@ from selenium import webdriver
 import selenium.webdriver.support.expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from os.path import exists
-from os import makedirs
-from os import listdir
-from shutil import rmtree
+from platform import system
 import time
-from PIL import Image
+import json
+import os
 
-# 初始化所需文件夹
-def init_folders():
-
-    for dir in ['img_data']:
-        if(not exists(dir)):
-            # 目录不存在时则创建
-            makedirs(dir)
-        else:
-            # 目录存在时则先删除递归删除该目录再重新创建
-            rmtree(dir)
-            makedirs(dir)
-
-
-# 拼接所有图片成一张长图
-def merge_picture():
-
-    # 获取img_dat目录下所有Png图像
-    ims = [Image.open('img_data/' + fn) for fn in listdir('img_data') if fn.endswith('.png')]
-
-    # 获取单张图片的尺寸
-    width, height = ims[0].size
-
-    # 创建一个空白的长图
-    result = Image.new(ims[0].mode, (width, height * len(ims)))
-
-    # 拼接图片
-    for i, im in enumerate(ims):
-        print(u'({}/{})正在拼接朋友圈数据中，请耐心等待……'.format(i+1, len(ims)))
-        result.paste(im, box=(0, i * height))
-
-    # 保存长图
-    print(u'拼接完成，正在保存到本地文件中，请耐心等待……')
-    result.save('朋友圈数据.png')
-    print(u'保存成功，文件名为[朋友圈数据.png]，存放于当前目录下')
-
+# 引入打开文件所用的库
+# Window与Linux和Mac OSX有所不同
+# lambda用来定义一个匿名函数，可实现类似c语言的define定义
+if('Windows' in system()):
+    # Windows
+    from os import startfile
+    open_pdf_file = lambda x : startfile(x)
+elif('Darwin' in system()):
+    # MacOSX
+    from subprocess import call
+    open_pdf_file = lambda x : call(["open", x])
+else:
+    # Linux
+    from subprocess import call
+    open_pdf_file = lambda x: call(["xdg-open", x])
 
 
 if __name__ == '__main__':
 
+
     # chromedriver驱动文件的位置，可输入绝对路径或者相对路径，./表示当前目录下
     # './chromedriver_win32.exe'表示当前目录下的chromedriver_win32.exe文件
     # 不同系统和不同chrome版本需要下载不同的chromedriver，请下载合适自己的版本
-    # 下载地址http://chromedriver.chromium.org/downloads
-    chromedriver_path = './chromedriver_mac64_74'
+    # chromedriver下载地址http://chromedriver.chromium.org/downloads
+    # 默认的chromedriver支持的Chrome版本为74
+    chromedriver_path = './chromedriver_win32_74.0.3729.6.exe'
 
     # 你的微信朋友圈数据地址，注意不要泄露给其他人
-    target_url = 'https://chushu.la/book/chushula-918509291'
+    # 在调试过程中，可以直接给target_url赋值
+    target_url = input(u'请输入你的微信书完整地址:')
+    # target_url = 'https://chushu.la/book/chushula-918509291'
 
 
-
-
-
-
-    # 初始化相关目录
-    init_folders()
-
-    # 屏蔽chrome的提示
     option = webdriver.ChromeOptions()
+    # 屏蔽chrome的提示
     option.add_argument('disable-infobars')
+    # 静默自动打印为高清PDF文件，并存储到os.getcwd()目录，也就是当前目录
+    appState = {
+        # 添加保存为pdf选项
+        "recentDestinations": [
+            {
+                "id": "Save as PDF",
+                "origin": "local"
+            }
+        ],
+        # 选择保存为pdf选项
+        "selectedDestinationId": "Save as PDF",
+        # 版本2
+        "version": 2,
+        # 不显示页眉页脚
+        "isHeaderFooterEnabled": False
+    }
+
+    profile = {
+        # 打印前置参数
+        'printing.print_preview_sticky_settings.appState': json.dumps(appState),
+        # 默认下载、打印保存路径
+        'savefile.default_directory': os.getcwd()
+    }
+
+    # 添加实验性质的设置参数
+    option.add_experimental_option('prefs', profile)
+
+    # 添加启动参数，后台静默打印
+    option.add_argument('--kiosk-printing')
+
 
     # 绑定Chrome和chromedriver，不同Chrome版本对应的chromedriver是不同的，请注意
     driver = webdriver.Chrome(executable_path=chromedriver_path, options=option)
 
     # 将浏览器最大化显示，使得截图效果更好
     driver.maximize_window()
+
     # 延迟2秒，给最大化过程一点时间
     time.sleep(2)
 
     # 模拟浏览指定网页
     driver.get(target_url)
 
-
-    # 开始无限翻页，直到最后翻到一页
     for i in range(0, 10000):
 
-        # 等待 下一月控件 出现
-        next_month = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button.next-month')))
-
-        # 等待 下一月控件 可见才能模拟点击，不可见的元素点击了会出错
-        while(True):
-            if(next_month.is_displayed() == True):
+        # 等待当前页面所有数据加载完毕，正常情况下数据加载完毕后，这个‘加载中’元素会隐藏起来
+        while (True):
+            loading_status = WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, 'div.j-save-popup.save-popup')))
+            if (loading_status.is_displayed() == False):
                 break
-
 
         # 隐藏导航栏，防止影响截图效果
         js = 'document.querySelector("body > header").style.display="none";'
         driver.execute_script(js)
 
+        # 等待 下一月控件 出现
+        next_month = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button.next-month')))
 
-        # 等待所有数据加载完毕，正常情况下数据加载完毕后，这个‘加载中’元素会隐藏起来
+        # 等待 下一月控件 可见才能模拟点击
         while(True):
-            loading_status = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.j-save-popup.save-popup')))
-            if(loading_status.is_displayed() == False):
+            if(next_month.is_displayed() == True):
                 break
 
+        # 模拟点击 下一月控件
+        time.sleep(0.5)
+        next_month.click()
 
 
-        # 由于网站的图片是懒加载形式，所以需要挨个定位到每张图片的位置使该图片加载成功
-        # 每次寻找类名为lazy-img的img元素集合，当元素集合至少存在一个元素，则定位到第一个元素
-        # 当元素集合不存在任何元素，则说明懒加载的图片已经没有了，可以退出循环了
-        while (True):
-            try:
-                lazy_img = driver.find_elements_by_css_selector('img.lazy-img')
-                js = 'document.getElementsByClassName("lazy-img")[0].scrollIntoView();'
-                driver.execute_script(js)
-                time.sleep(0.2)
-            except:
-                # 找不到控件img.lazy-img，所以退出循环
-                break
-
-
-        # 等待 主页面控件 出现。这里主页面控件绝大部分情况下是早就加载完毕了，但是为了保险起见，多增加这层等待
-        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'ul.main')))
-        main = driver.find_element_by_css_selector('ul.main')
-        element_left_list = main.find_elements_by_css_selector('div.con-left')
-        element_right_list = main.find_elements_by_css_selector('div.con-right')
-
-
-        # 稍微延迟下，给懒加载的图片完全显示提供一点时间
-        time.sleep(2)
-
-
-        # 开始截图朋友圈数据元素，分为左右页
-        for index, element in enumerate(element_left_list):
-            if(element.is_displayed() == True):
-                element.screenshot('img_data/' + '{}-{}-0.png'.format(i, index))
-
-        for index, element in enumerate(element_right_list):
-            if(element.is_displayed() == True):
-                element.screenshot('img_data/' + '{}-{}-1.png'.format(i, index))
-
-
-        # 判断当下一月控件的class name 是否为next-month disable，如果是，则说明翻到最后一页了，可以跳出循环了，否则模拟点击下一月控件
-        page_source=driver.page_source
+        # 判断当下一月控件的class name 是否为next-month disable，如果是，则说明翻到最后一月了
+        page_source = driver.page_source
         if('next-month disable' in page_source):
+
+            # 等待当前页面所有数据加载完毕，正常情况下数据加载完毕后，这个‘加载中’元素会隐藏起来
+            while (True):
+                loading_status = WebDriverWait(driver, 20).until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, 'div.j-save-popup.save-popup')))
+                if (loading_status.is_displayed() == False):
+                    break
+
+
+            # 等待 主页面控件 出现
+            WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'ul.main')))
+            main = driver.find_element_by_css_selector('ul.main')
+            element_left_list = main.find_elements_by_css_selector('div.con-left')
+
+
+            # 每一个element代表每一页，将每一页中style的display属性改成block，即可见状态
+            for index, element in enumerate(element_left_list):
+                # ..在xpath中表示上一级的元素，也就是父元素
+                parent_element = element.find_element_by_xpath('..')
+                # 获取这个父元素的完整id
+                parent_element_id = parent_element.get_attribute('id')
+
+                # 将该父元素更改为可见状态
+                js = 'document.getElementById("{}").style.display="block";'.format(parent_element_id)
+                driver.execute_script(js)
+
+                # 将每一页之间的间隔去掉
+                js = 'document.getElementById("{}").style.marginTop="0px";'.format(parent_element_id)
+                driver.execute_script(js)
+
+
+
+            # 由于网站的图片是懒加载形式，所以需要挨个定位到每张图片的位置
+            # 每次寻找是否存在类名为lazy-img的img元素集合，当元素集合至少存在一个元素，则定位到第一个元素
+            # 当元素集合不存在任何元素，则说明懒加载的图片已经没有了，可以退出循环了
+            while(True):
+                try:
+                    lazy_img = driver.find_elements_by_css_selector('img.lazy-img')
+                    js = 'document.getElementsByClassName("lazy-img")[0].scrollIntoView();'
+                    driver.execute_script(js)
+                    time.sleep(3)
+                except:
+                    # 找不到控件img.lazy-img，所以退出循环
+                    break
+
+
             break
 
 
-        # 截图完这一月的数据后，模拟点击 下一月控件
-        next_month.click()
-        # 稍微延时
-        time.sleep(1)
 
 
+    # 调用chrome打印功能
+    driver.execute_script('window.print();')
 
-    # 提示
-    print(u'翻页完毕')
 
-    # 延时1秒
-    time.sleep(1)
+    # 调用系统方式自动打开这个pdf文件
+    pdf_file_name = '{}.pdf'.format(driver.title)
+    print(u'生成完毕，已为你自动打开 {}'.format(pdf_file_name))
+    open_pdf_file(pdf_file_name)
 
-    # 拼接所有图片成一张长图
-    merge_picture()
-
-    # 提示
-    print(u'拼接完毕')
-
-    # 关闭浏览器
+    # 退出浏览器
     driver.quit()
