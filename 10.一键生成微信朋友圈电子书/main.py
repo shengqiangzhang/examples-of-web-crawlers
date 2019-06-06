b20ced0c6eb20daceefac99a216b61a6cb5ad05a
sqzhang <3257179914@qq.com>
2019-06-06 14:01:46 +0800

    fix some bugs

--- 10.一键生成微信朋友圈电子书/main.py
+++ 10.一键生成微信朋友圈电子书/main.py
@@ -8,25 +8,31 @@ import time
 
 
 if __name__ == '__main__':
-    # http://chromedriver.chromium.org/downloads
-    driver = webdriver.Chrome(executable_path='./chromedriver_mac64_74')
+
+    # 屏蔽chrome提示
+    option = webdriver.ChromeOptions()
+    option.add_argument('disable-infobars')
+
+    # 不同系统和不同chrome版本下载不同的chromedriver，请下载合适的版本
+    # 下载地址http://chromedriver.chromium.org/downloads
+    driver = webdriver.Chrome(executable_path='./chromedriver_mac64_74', options=option)
+
     # 将浏览器最大化显示
     driver.maximize_window()
+    # 延迟2秒
     time.sleep(2)
+
+    # 浏览网页
     driver.get('https://chushu.la/book/chushula-918509291')
-    driver.implicitly_wait(60)
-    print(u'页面加载完毕')
 
 
-
-
-
-
+    # 无限翻页，直到最后翻到一页
     for i in range(0, 10000):
 
         # 等待 下一月控件 出现
         next_month = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button.next-month')))
-        # 等待 下一月控件 可见才能模拟点击
+
+        # 等待 下一月控件 可见才能模拟点击，不可见的元素点击了会出错
         while(True):
             if(next_month.is_displayed() == True):
                 break
@@ -36,30 +42,41 @@ if __name__ == '__main__':
         js = 'document.querySelector("body > header").style.display="none";'
         driver.execute_script(js)
 
-        # 获取页面高度，以便知道要滑动多少次
-        html = driver.find_element_by_css_selector('html')
-        html_height = (html.size)['height']
 
-        # 模拟人工向下滑动，使得图片能够加载
-        # 经过查看源代码分析得知，每一小页的高度为780px
-        for j in range(0, 999):
-            position = 780 * (j+1)
-            js = "var q=document.documentElement.scrollTop=" + str(position)
-            driver.execute_script(js)
-            time.sleep(3)
-
-            if(position > html_height):
+        # 等待所有数据加载完毕，正常情况下数据加载完毕后，这个‘加载中’元素会隐藏起来
+        while(True):
+            loading_status = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.j-save-popup.save-popup')))
+            if(loading_status.is_displayed() == False):
                 break
 
 
 
-        # 等待 主页面控件 出现。大部分情况下，经常上面的滚动操作，主页面控件都是已经出现了的。
+        # 由于网站的图片是懒加载形式，所以需要挨个定位到每张图片的位置使该图片加载成功
+        # 每次寻找类名为lazy-img的img元素集合，当元素集合至少存在一个元素，则定位到第一个元素
+        # 当元素集合不存在任何元素，则说明懒加载的图片已经没有了，可以退出循环了
+        while (True):
+            try:
+                lazy_img = driver.find_elements_by_css_selector('img.lazy-img')
+                js = 'document.getElementsByClassName("lazy-img")[0].scrollIntoView();'
+                driver.execute_script(js)
+                time.sleep(0.2)
+            except:
+                # 找不到控件img.lazy-img，所以退出循环
+                break
+
+
+        # 等待 主页面控件 出现。这里主页面控件绝大部分情况下是早就加载完毕了，但是为了保险起见，多增加这层等待
         WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'ul.main')))
         main = driver.find_element_by_css_selector('ul.main')
         element_left_list = main.find_elements_by_css_selector('div.con-left')
         element_right_list = main.find_elements_by_css_selector('div.con-right')
 
 
+        # 稍微延迟下，给懒加载的图片完全显示提供一点时间
+        time.sleep(2)
+
+
+        # 开始截图朋友圈数据元素，分为左右页
         for index, element in enumerate(element_left_list):
             if(element.is_displayed() == True):
                 element.screenshot('{}-{}-left.png'.format(i, index))
@@ -69,23 +86,24 @@ if __name__ == '__main__':
                 element.screenshot('{}-{}-right.png'.format(i, index))
 
 
-
-
-
-        # 模拟点击 下一月控件
-        next_month.click()
-        # 延时
-        time.sleep(1)
-
-        # 判断当下一月控件的class name 是否为next-month disable，如果是，则说明翻到最后一页了，可以结束了
+        # 判断当下一月控件的class name 是否为next-month disable，如果是，则说明翻到最后一页了，可以跳出循环了，否则模拟点击下一月控件
         page_source=driver.page_source
         if('next-month disable' in page_source):
             break
 
 
+        # 截图完这一月的数据后，模拟点击 下一月控件
+        next_month.click()
+        # 稍微延时
+        time.sleep(1)
+
+
+
+    # 提示
     print(u'翻页完毕')
-    js = 'alert("下载完毕")'
-    driver.execute_script(js)
+
+    # 延时1秒
+    time.sleep(1)
 
     # 关闭浏览器
-    # driver.quit()
+    driver.quit()
--- 10.一键生成微信朋友圈电子书/main1.py
+++ 10.一键生成微信朋友圈电子书/main1.py
@@ -12,20 +12,17 @@ if __name__ == '__main__':
     driver = webdriver.Chrome(executable_path='./chromedriver_mac64_74')
     # 将浏览器最大化显示
     driver.maximize_window()
-    time.sleep(2)
+    time.sleep(1)
+
+    # 跳转到指定网址
     driver.get('https://chushu.la/book/chushula-918509291')
-    driver.implicitly_wait(60)
-    print(u'页面加载完毕')
-
-
-
-
 
 
     for i in range(0, 10000):
 
         # 等待 下一月控件 出现
         next_month = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button.next-month')))
+
         # 等待 下一月控件 可见才能模拟点击
         while(True):
             if(next_month.is_displayed() == True):
@@ -39,14 +36,15 @@ if __name__ == '__main__':
 
         # 模拟点击 下一月控件
         next_month.click()
+
         # 延时
         time.sleep(1)
 
-        # 判断当下一月控件的class name 是否为next-month disable，如果是，则说明翻到最后一页了
-        page_source=driver.page_source
+        # 判断当下一月控件的class name 是否为next-month disable，如果是，则说明翻到最后一月了
+        page_source = driver.page_source
         if('next-month disable' in page_source):
 
-            # 等待 主页面控件 出现。大部分情况下，经常上面的滚动操作，主页面控件都是已经出现了的。
+            # 等待 主页面控件 出现
             WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'ul.main')))
             main = driver.find_element_by_css_selector('ul.main')
             element_left_list = main.find_elements_by_css_selector('div.con-left')
@@ -64,23 +62,20 @@ if __name__ == '__main__':
                 driver.execute_script(js)
 
 
-            # 获取页面高度，以便知道要滑动多少次才能到底部
-            time.sleep(3)
-            html = driver.find_element_by_css_selector('html')
-            html_height = (html.size)['height']
-            print(html.size)
-
-
-            # 模拟人工向下滑动，使得图片能够加载
-            # 经过查看源代码分析得知，每一小页的高度为780px
-            for j in range(0, 999):
-                position = 780 * (j+1)
-                js = "var q=document.documentElement.scrollTop=" + str(position)
-                driver.execute_script(js)
-                time.sleep(3)
-
-                if(position > html_height):
+            # 由于网站的图片是懒加载形式，所以需要挨个定位到每张图片的位置
+            # 每次寻找是否存在类名为lazy-img的img元素集合，当元素集合至少存在一个元素，则定位到第一个元素
+            # 当元素集合不存在任何元素，则说明懒加载的图片已经没有了，可以退出循环了
+            while(True):
+                try:
+                    lazy_img = driver.find_elements_by_css_selector('img.lazy-img')
+                    js = 'document.getElementsByClassName("lazy-img")[0].scrollIntoView();'
+                    driver.execute_script(js)
+                    time.sleep(0.2)
+                except:
+                    # 找不到控件img.lazy-img，所以退出循环
                     break
+
+
 
 
             # 开始截取元素
@@ -88,18 +83,20 @@ if __name__ == '__main__':
             element_right_list = main.find_elements_by_css_selector('div.con-right')
             for index, element in enumerate(element_left_list):
                 if(element.is_displayed() == True):
-                    element.screenshot('{}-{}-left.png'.format(i, index))
+                    print(element.get_attribute('class'))
+                    # element.screenshot('{}-{}-0.png'.format(i, index))
+                    # time.sleep(1)
 
             for index, element in enumerate(element_right_list):
                 if(element.is_displayed() == True):
-                    element.screenshot('{}-{}-right.png'.format(i, index))
+                    print(element.get_attribute('class'))
+                    # element.screenshot('{}-{}-1.png'.format(i, index))
+                    # time.sleep(1)
 
             break
 
 
     print(u'翻页完毕')
-    js = 'alert("下载完毕")'
-    driver.execute_script(js)
 
 
     #关闭浏览器
